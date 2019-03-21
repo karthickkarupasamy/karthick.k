9f1982d4a2082e623c3ff1a903af1e275a7375c6
karthickkarupasamy <14101995karthick@gmail.com>
2019-03-21 16:06:41 +0530

    Create 33.c

---
+++ player/set1/33.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+  char a[10000];
+  int i,l;
+  
+  scanf("%s",a);
+
+
+  l=strlen(a);
+  for(i=l-1;i>=0;i--)
+
+  {
+    printf("%c",a[i]);
+
+  }
+}
