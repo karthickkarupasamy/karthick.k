1dbbafd4bf617d294273bf7bae5cc152b5ed27f6
karthickkarupasamy <14101995karthick@gmail.com>
2019-03-25 11:51:20 +0530

    Create big3.c

---
+++ big3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{char ch;
+printf("enter the letter");
+scanf("%c",&ch);
+if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+{
+printf("vowel",ch);
+}
+else
+{ 
+printf("consontant",ch);
+}
+}
