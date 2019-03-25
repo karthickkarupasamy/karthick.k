16edc6aad060202b6032d17b9864d705c67d1c60
karthickkarupasamy <14101995karthick@gmail.com>
2019-03-25 11:54:19 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int main(void)
+int main(void) 
+  
 {char ch;
 printf("enter the letter");
 scanf("%c",&ch);
+ 
 if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
-{
-printf("vowel",ch);
+{printf("vowel",ch);
 }
-else
-{ 
+else{ 
 printf("consontant",ch);
 }
 }
