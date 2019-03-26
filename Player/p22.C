593a825c600d8da7bde2ce465765cd9fa9745eac
karthickkarupasamy <14101995karthick@gmail.com>
2019-03-26 16:12:17 +0530

    Create p22.C

---
+++ Player/p22.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void) 
+{
+int n,k,i,l;
+scanf("%d %d",&n,&k);
+if(n>k)
+{l=n;
+}
+else
+{
+l=k;
+}
+for(i=l;i>=2;i--)
+{
+if(n%i==0 && k%i==0)
+{
+printf("%d\n ",i);		
+}}
+return 0;
+}
