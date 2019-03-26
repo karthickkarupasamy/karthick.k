7b2e9c690666c67adf8a1cdc6fd2f497a19feb77
karthickkarupasamy <14101995karthick@gmail.com>
2019-03-26 19:16:10 +0530

    Create p26.C

---
+++ Player/p26.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+char a[1000];
+int i,j;
+gets(a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ' && a[i+1]==' ')
+{
+for(j=i;a[j]!='\0';j++)
+{
+a[j+1]=a[j+2];
+}}
+if(a[i]==' ' && a[i-1]==' ')
+for(j=i-1;a[j]!='\0';j++)
+{
+a[j+1]=a[j+2];
+}}
+puts(a);
+return 0;
+}
