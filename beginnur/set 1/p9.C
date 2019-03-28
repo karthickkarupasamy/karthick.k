9165ed66c8745edd033acd2997d482e7f2f2e9a3
karthickkarupasamy <14101995karthick@gmail.com>
2019-03-28 16:14:23 +0530

    Create p9.C

---
+++ beginnur/set 1/p9.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main() 
+{
+int i,n,k,a[5],b=0;
+
+scanf("%d,%d",&n,&k);
+for(i=1;i<=n;i++)
+{
+scanf("\n%d",&a[i]);
+}
+for(i=1;i<=k;i++)
+{b=b+a[i];
+}
+printf("%d",b);
+return 0;
+}
